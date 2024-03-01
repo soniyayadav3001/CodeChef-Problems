185580e9163b47e6827b54e3fac106e75bb93b49
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-01 20:14:57 +0530

    Create Chef And Stock Prices.cpp

---
+++ Chef And Stock Prices.cpp
@@ -0,0 +1,30 @@
+//Problem:https://www.codechef.com/practice/course/1to2stars/LP1TO201/problems/CSTOCK?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t, a, b;
+	
+	float s, c;
+	
+	cin>>t;
+	
+	while(t--)
+	{
+	    cin>>s>>a>>b>>c;
+	    
+	    s = s+(s*(c/100.0));
+	    
+	    if(s>=a && s<=b)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
