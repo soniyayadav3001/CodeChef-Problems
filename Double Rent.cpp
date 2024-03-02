97faa1b5abc053e587885f2573f5562d344de820
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-02 19:38:02 +0530

    Create Double Rent.cpp

---
+++ Double Rent.cpp
@@ -0,0 +1,11 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    cout<<2*x;
+}
