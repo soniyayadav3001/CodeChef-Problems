6420d1782cb654ed300cacfb7839c3b1a5468d06
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-03 21:50:01 +0530

    Create Off By One.cpp

---
+++ Off By One.cpp
@@ -0,0 +1,11 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x , y;
+cin>>x>>y;
+cout<<10*(x+y)+1<<endl;
+}
