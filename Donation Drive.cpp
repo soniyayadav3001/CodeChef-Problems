c03e6e4911a876ea5032485b416fdca03a68ec8b
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-03 21:52:59 +0530

    Create Donation Drive.cpp

---
+++ Donation Drive.cpp
@@ -0,0 +1,15 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n , x, y;
+	cin>>n;
+	while(n--){
+	   cin>>x>>y;
+	   cout<<x-y<<endl;
+	}
+
+}
