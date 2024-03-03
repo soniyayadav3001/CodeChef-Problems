c611f59264c12b3eff1adbc9c5b40b1ad4836f27
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-03 21:42:48 +0530

    Create 404 Not Found.cpp

---
+++ 404 Not Found.cpp
@@ -0,0 +1,16 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x;
+    cin>>x;
+    if(x==404){
+        cout<<"NOT FOUND"<<endl;
+    }
+    else{
+        cout<<"FOUND"<<endl;
+    }
+}
