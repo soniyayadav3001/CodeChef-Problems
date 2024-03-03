c4379559657489f91e40f821386bb15009717bdb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-03 21:28:31 +0530

    Create How many unattempted problems.cpp

---
+++ How many unattempted problems.cpp
@@ -0,0 +1,11 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x , y;
+    cin>>x>>y;
+    cout<<x-y<<endl;
+}
