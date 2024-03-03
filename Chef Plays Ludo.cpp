b04880748d3d34d4556aabbb54c12e3387a91098
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-03 21:25:24 +0530

    Create Chef Plays Ludo.cpp

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,20 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUDO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t , x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x==6){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
