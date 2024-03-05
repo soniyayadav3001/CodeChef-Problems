e6c5a4abf88d47a10724e967d38cb21531b52346
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-05 19:57:46 +0530

    Create Reach on Time.cpp

---
+++ Reach on Time.cpp
@@ -0,0 +1,19 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TIMELY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t ,x;
+    cin>>t;
+    while(t--){
+        cin>>x;
+        if(x>=30){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
