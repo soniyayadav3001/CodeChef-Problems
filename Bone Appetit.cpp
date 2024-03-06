9cc7bc73cd25042bf0a6eb763c9376f79019177c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-06 20:50:39 +0530

    Create Bone Appetit.cpp

---
+++ Bone Appetit.cpp
@@ -0,0 +1,12 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n , m  , x , y;
+cin>>n>>m;
+cin>>x>>y;
+cout<<n*x + m*y<<endl;
+}
