930129e210e24b8f2f78561aa163758dcf6eefc3
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-06 20:44:32 +0530

    Create Puzzle Hunt.cpp

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,16 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n ;
+cin>>n;
+if(n>=6&&n<=8){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;
+}
+}
