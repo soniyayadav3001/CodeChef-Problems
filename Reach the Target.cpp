cdbd869ce6d00cc595f4eae9adc4a6d7fd5d3a6c
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-07 20:21:47 +0530

    Create Reach the Target.cpp

---
+++ Reach the Target.cpp
@@ -0,0 +1,14 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACHTARGET
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t , x , y;
+cin>>t;
+while(t--){
+    cin>>x>>y;
+    cout<<x-y<<endl;
+}
+}
