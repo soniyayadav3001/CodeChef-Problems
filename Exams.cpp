67374fdfa4a17ab75dddd6ab2103b2c9867a35a1
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-11 20:51:11 +0530

    Create Exams.cpp

---
+++ Exams.cpp
@@ -0,0 +1,17 @@
+//Problem : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXAMCHEF?tab=statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin >> x >> y >> z;
+	    int students = x*y;
+	    z > students/2 ? cout<<"YES" <<endl : cout <<"NO"<<endl;
+	}
+	return 0;
+}
