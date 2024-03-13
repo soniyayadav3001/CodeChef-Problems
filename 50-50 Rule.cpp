8f485202e864889de4216cd96f5bb93c73371e18
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-13 20:57:36 +0530

    Create 50-50 Rule.cpp

---
+++ 50-50 Rule.cpp
@@ -0,0 +1,24 @@
+//CodeChef contest problem 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+int x , y;
+cin>>x>>y;
+if(x<50){
+    cout<<"z"<<endl;
+}
+else if(y<50){
+    cout<<"f"<<endl;
+}
+else{
+    cout<<"A"<<endl;
+}
+	    
+	}
+}
