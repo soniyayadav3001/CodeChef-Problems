c8ae158c1723f725a241f5a9ab91cd3c7cfc727e
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-14 20:39:20 +0530

    Create Best of Two.cpp

---
+++ Best of Two.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BESTOFTWO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t,x,y;
+cin>>t;
+while(t--){
+    cin>>x>>y;
+    if(x>y){
+        cout<<x<<endl;
+    }
+    else if(y>x){
+        cout<<y<<endl;
+    }
+    else if(x==y){
+        cout<<x<<endl;
+    }
+}
+}
