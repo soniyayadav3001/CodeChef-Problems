1d4abccdb45fd6e7d48f624c95dd92e35d943a8e
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-15 20:27:42 +0530

    Create Janmansh and Assignments.cpp

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,19 @@
+//Problem : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t , x;
+cin>>t;
+while(t--){
+    cin>>x;
+    if(x+3<=10){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
+}
