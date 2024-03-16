9511df5237b929be2edfa372bc466742b8384acb
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-16 18:34:40 +0530

    Create Chef in his Office.cpp

---
+++ Chef in his Office.cpp
@@ -0,0 +1,14 @@
+//Problem : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t , x,y;
+    cin>>t;
+    while(t--){
+        cin>>x>>y;
+        cout<<x*4+y<<endl;
+    }
+}
