17e1a31e23464f3703a4a0effb4a1e825a66fdc9
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-20 20:39:41 +0530

    Create AC Please.cpp

---
+++ AC Please.cpp
@@ -0,0 +1,16 @@
+//CodeChef Contest Problem 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t ;
+cin>>t;
+if(t>30){
+    cout<<"YES"<<endl;
+}
+else{
+    cout<<"NO"<<endl;
+}
+}
