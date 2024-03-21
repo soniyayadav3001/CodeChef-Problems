86528996a7959eed6e811f05f33298d387f3c4db
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-21 20:42:02 +0530

    Create 2000.cpp

---
+++ 2000.cpp
@@ -0,0 +1,11 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n;
+	cin>>n;
+	cout << (n*2000)/500;
+}
