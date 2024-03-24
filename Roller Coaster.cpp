178f3f122324372e56e22f30042def2d4131c93e
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-24 20:07:33 +0530

    Create Roller Coaster.cpp

---
+++ Roller Coaster.cpp
@@ -0,0 +1,19 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINHEIGHT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t , x , y;
+cin>>t;
+while(t--){
+    cin>>x>>y;
+    if(x>=y){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+}
