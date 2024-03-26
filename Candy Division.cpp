6906151e350e186e07d8827e5b225a5143565302
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-26 19:19:36 +0530

    Create Candy Division.cpp

---
+++ Candy Division.cpp
@@ -0,0 +1,18 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CANDIVIDE?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%3==0)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+}
