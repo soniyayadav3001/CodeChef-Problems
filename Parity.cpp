b21560717b34e1d70d41f827301bf501d2b57b9f
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-28 20:28:37 +0530

    Create Parity.cpp

---
+++ Parity.cpp
@@ -0,0 +1,19 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PAR2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t , n;
+cin>>t;
+while(t--){
+    cin>>n;
+    if(n%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+}
