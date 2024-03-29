4fe59da3616aa4d41318c42dd54ab71f49b70ee5
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-03-29 20:00:05 +0530

    Create Total Prize Money.cpp

---
+++ Total Prize Money.cpp
@@ -0,0 +1,14 @@
+//Problem : https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRIZEPOOL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t , x , y;
+cin>>t;
+while(t--){
+    cin>>x>>y;
+    cout<<10*x+90*y<<endl;
+}
+}
