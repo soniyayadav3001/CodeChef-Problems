93474e04a5ab1367d8dd0673f9931fb4d84037f1
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-04-10 20:29:37 +0530

    Create Football Training.cpp

---
+++ Football Training.cpp
@@ -0,0 +1,18 @@
+//Problem : https://www.codechef.com/START129D/problems/MESSI
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x,y;
+
+    cin>>x>>y;
+    if(x>y){
+        cout<<"FREEKICK"<<endl;
+    }
+    else{
+        cout<<"PENALTY"<<endl;
+    
+}
+}
