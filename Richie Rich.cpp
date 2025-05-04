fb07a1a58151006ceab02a19367876105165b4f2
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2025-05-04 19:10:15 +0530

    Create Richie Rich.cpp

---
+++ Richie Rich.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+    cin >> T;  
+    while (T--) {
+        int A, B, X;
+        cin >> A >> B >> X;
+        cout << (B - A) / X << endl;
+    }
+}
